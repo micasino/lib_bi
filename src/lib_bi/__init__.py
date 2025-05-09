e092f700a8f74458ce1138639dd432fe4c4b0ae8
betzamica <betzabeth.maestre@micasino.com>
2025-05-09 01:48:46 +0000

    add name to the init

--- src/lib_bi/__init__.py
+++ src/lib_bi/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from lib-bi!"
+from .gcp_bq_handler import GCPBigQueryHandler as GCPBigQueryHandler
+from .google_drive_handler import GoogleDriveHandler as GoogleDriveHandler
+from .replica_handler import ReplicaHandler as ReplicaHandler
+from .sftp_handler import SftpHandler as SftpHandler
+from .ftp_handler import FtpHandler as FtpHandler
